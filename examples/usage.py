b8285421197c7117330829d9b0a39dbc1d9b80b3
Payne <1289690157@qq.com>
2020-11-12 23:09:11 +0800

    "Update"

--- examples/usage.py
+++ examples/usage.py
@@ -1,5 +1,29 @@
-from powerspider.Download import downloader
+# from powerspider.Download import downloader
+#
+# if __name__ == '__main__':
+#     # print(downloader("https://www.baidu.com/", "GET"))
+#     downloader("https://www.baidu.com/", "GET")
 
-if __name__ == '__main__':
-    # print(downloader("https://www.baidu.com/", "GET"))
-    downloader("https://www.baidu.com/", "GET")
+import requests
+import cchardet
+
+#
+# print("response.status_code:", response.status_code)
+# print("response.headers:", response.headers)
+# print("response.request.headers:", response.request.headers)
+# print("response.content:", response.content)
+# print("response.text", response.text)
+
+urls = 'http://httpbin.org/get'
+from powerspider.tools.Ua import ua
+
+
+def downloader(url, method=None, header=None, binary=False):
+    _headers = header if header else {'User-Agent': ua()}
+    _method = "GET" if not method else method
+    response = requests.request(url, method=_method, headers=_headers)
+    encoding = cchardet.detect(response.content)['encoding']
+    return response.content if binary else response.content.decode(encoding)
+
+
+print(downloader(urls, method="POST"))
---
+++ powerspider/basicSpider.py
@@ -0,0 +1,16 @@
+import cchardet
+from retrying import retry
+from powerspider import logger
+from powerspider.tools.Ua import ua
+from requests import request, RequestException
+
+
+class BasicSpider:
+    def __init__(self):
+        pass
+
+    def downloader(self):
+        pass
+
+    def download(self):
+        pass
