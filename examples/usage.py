613dbf1fd7c18323541f98d94d79a8cc7018aaac
Payne <1289690157@qq.com>
2020-12-11 19:43:20 +0800

    fix input

--- examples/usage.py
+++ examples/usage.py
@@ -3,8 +3,3 @@ from powerspider.Download import downloader
 if __name__ == '__main__':
     # print(downloader("https://www.baidu.com/", "GET"))
     response = downloader("https://www.baidu.com/", "GET")
-    print("response.status_code:", response.status_code)
-    print("response.headers:", response.headers)
-    print("response.request.headers:", response.request.headers)
-    print("response.content:", response.content)
-    print("response.text", response.text)
